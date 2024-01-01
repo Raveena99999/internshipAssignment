89dd121cc011b01501b363734961b7488ef94c4f
Raveena Banduni <raveenabanduni06@gmail.com>
2024-01-01 19:24:04 +0530

    completed with few changes in internship swipper assignment

--- src/components/AccordianComponent.jsx
+++ src/components/AccordianComponent.jsx
@@ -86,6 +86,56 @@ const AccordianComponent = () => {
             </>
           )}
         </AccordionItem>
+
+        <AccordionItem>
+          {({ isExpanded }) => (
+            <>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                  Will investor be able to have multiple Common Account
+                  </Box>
+                  {isExpanded ? (
+                    <Image src={minus} w="1.5rem" />
+                  ) : (
+                    <Image src={plus} w="1.5rem" />
+                  )}
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </AccordionPanel>
+            </>
+          )}
+        </AccordionItem>
+
+        <AccordionItem>
+          {({ isExpanded }) => (
+            <>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    How does an investor gain access to MF utility?
+                  </Box>
+                  {isExpanded ? (
+                    <Image src={minus} w="1.5rem" />
+                  ) : (
+                    <Image src={plus} w="1.5rem" />
+                  )}
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </AccordionPanel>
+            </>
+          )}
+        </AccordionItem>
       </Accordion>
     </>
   );
